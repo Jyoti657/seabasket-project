def88236d416e2bca9134c57cb4f946cd66c8585
mourya jyoti <mouryajyoti657@gmail.com>
2025-04-19 11:45:15 +0530

    feat: add loading spinner to HomePage and implement loading state management

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,29 @@
-import CategoriesListProduct from "../components/header/CategoriesListProduct";
+import { useEffect, useState } from "react";
 import CategoryList from "../components/header/CategoryList";
 import TrendingProducts from "../components/header/TrendingProducts";
+import Spinner from "../components/ui/Spinner";
 
 const Home: React.FC = () => {
+ const[loading,setLoading]= useState(true)
+ useEffect(()=>{
+  const timer=setTimeout(()=>{
+    setLoading(false)
+  }
+  ,2000)
+  return () => clearTimeout(timer)
+ },[])
+ if(loading){
+  return <Spinner/>
+ }
   return (
     <div className="min-h-screen flex flex-col items-center">
-      <div> 
-        <TrendingProducts/>
-        
-        </div>
+      <div>
+        <TrendingProducts />
+      </div>
       <div>
         <CategoryList />
       </div>
-       {/* < className="flex flex-col items-center">
-        <h2 className="font-semibold mb-5 text-xl">Categories</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
-  {/* <CategoriesListProduct /> 
-</div> */}
-
-      </div> 
-    
+    </div>
   );
 };
 
